ef07a1d2d68395b7911bce7ef5afcb60cbcf8f69
bekhtin_av <bekhtin_av_10_20@gmail.ru>
2020-11-10 12:42:28 +0300

    On branch master .cpp

---
+++ Z.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, c;
+    cin >> a;
+    int n1 = 0, n2 = 1;
+    for (int i = 0; i < a; i++) {
+        cout << n2 << " ";
+        c = n2-n1;
+        n2 = n2 + n1;
+        n1 += c;
+    }
+    return 0;
+}
+
